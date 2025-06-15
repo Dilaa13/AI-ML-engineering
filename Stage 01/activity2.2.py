17ed45e67afdd48f9acfc1f7ad337e37e231f4b9
Charith Dhananjaya <charithdhanam@gmail.com>
2025-06-15 22:39:28 +0530

    Activity2.2-done

---
+++ Stage 01/activity2.2.py
@@ -0,0 +1,8 @@
+x = float(input("Length of the rectangle: "))
+y = float(input("Width of the rectangle: "))
+
+area = x*y
+circum = 2*(x+y)
+
+print("The Area of the rectangle is : " , area , "cm^2")
+print("The Circumferance of the rectangle is : " , circum , "cm")
