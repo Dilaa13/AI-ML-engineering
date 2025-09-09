0bb984d44735561a7c9ee67bb964a7d06dc2e1a6
Charith Dhananjaya <charithdhanam@gmail.com>
2025-09-09 10:51:26 +0530

    dictionaries

---
+++ Stage 01/Topic 2.3/diction.py
@@ -0,0 +1,8 @@
+thisdict = {"id" : 1313 , "name" : "Charith" , "salary" : 500000}
+
+print(thisdict)
+print(type(thisdict))
+
+print(thisdict["id"])
+print(thisdict["name"])
+print(thisdict["salary"])
